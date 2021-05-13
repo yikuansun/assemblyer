79544bd4c663ddb2757381400f8fe59cdb041a12
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-13 17:02:56 -0400

    Made a compiler

---
+++ code.js
@@ -0,0 +1,21 @@
+function compile(code) {
+    const availOpcodes = ["LOAD", "STORE", "ADD", "SUB", "MULT", "DIV", "BG", "BE", "BL", "BU", "READ", "PRINT", "DC", "END"];
+
+    var lines = code.split("\n");
+    var compiledObject = {
+        lines: [],
+        labels: {}
+    };
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        if (line.split(" ").length < 3) return `Compiling error in line ${i + 1}: Format does not match LABEL OPCODE LOC [COMMENTS]`;
+        if (!availOpcodes.includes(line.split(" ")[1])) return `Compiling error in line ${i + 1}: OPCODE unknown`;
+        compiledObject.lines.push({
+            label: line.split(" ")[0],
+            opcode: line.split(" ")[1],
+            loc: line.split(" ")[2],
+        });
+        if (line.split(" ")[0]) compiledObject.labels[line.split(" ")[0]] = i;
+    }
+    return compiledObject;
+}
